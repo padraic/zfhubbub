d065223941857168ae94b5b28840f618cea94274
Pádraic Brady <padraic@padraic-desktop.(none)>
2009-10-14 09:08:33 +0100

    DEBUG

--- application/modules/admin/controllers/SubscribeController.php
+++ application/modules/admin/controllers/SubscribeController.php
@@ -69,6 +69,9 @@ class Admin_SubscribeController extends Zend_Controller_Action
         /**
          * Do some checking for errors...
          */
+        if (!$subscriber->isSuccess()) {
+            var_dump($subscriber->getErrors()); exit;
+        }
         return true;
     }
 
